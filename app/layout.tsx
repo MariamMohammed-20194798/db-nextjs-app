90fc7ba8f7711d9c35db492cc7b22d6cdd780dff
Mariam Mohammed <74053841+MariamMohammed-20194798@users.noreply.github.com>
2026-05-15 11:48:20 +0300

    fixwd Hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full" suppressHydrationWarning>
       <head>
         <SystemThemeInitializer />
       </head>
